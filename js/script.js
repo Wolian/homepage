1c81c0b2774d8698517ffb629c9d0d1af2730d88
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-04-02 15:48:53 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-console.log("Piątka dla ciebie jeśli tu jesteś!");
-
-
-let main__button = document.querySelector(".main__button");
-let body = document.querySelector(".body");
-let main__nextColorName = document.querySelector(".main__nextColorName");
-
-main__button.addEventListener("click", () => {
-
-    body.classList.toggle("darkBackground");
-    if (body.classList.contains("darkBackground")) {
-        main__nextColorName.innerText = "jasny";
-    } else {
-        main__nextColorName.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Piątka dla ciebie jeśli tu jesteś!");
     }
 
-});
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const main__nextColorName = document.querySelector(".main__nextColorName");
 
+        body.classList.toggle("darkBackground");
+        if (body.classList.contains("darkBackground")) {
+            main__nextColorName.innerText = "jasny";
+        } else {
+            main__nextColorName.innerText = "ciemny";
+        }
+
+    };
+
+    const init = () => {
+        let main__button = document.querySelector(".main__button");
+        main__button.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+
+    init();
+
+}
